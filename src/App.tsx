921115c782780bc58aedb0bd5147f89e8ca6519a
yurim <kyl6682@gmail.com>
2025-04-28 19:59:37 +0900

    style : tailwind content 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
     <>
-      <div className='font-pretendard text-primary-800'>Hello</div>
+      <div className="font-pretendard text-primary-800">Hello</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
