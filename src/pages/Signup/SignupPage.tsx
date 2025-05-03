48c62d020f6c662ce8868240dfe43311428e0013
yurim <kyl6682@gmail.com>
2025-05-03 20:56:58 +0900

    style: #19 페이지 min height 수정

--- src/pages/Signup/SignupPage.tsx
+++ src/pages/Signup/SignupPage.tsx
@@ -3,7 +3,7 @@ import SignupForm from './components/SignupForm';
 
 const SignupPage = () => {
   return (
-    <div className="flex flex-col items-center h-full bg-gray-200">
+    <div className="flex flex-col items-center h-full min-h-screen bg-gray-200">
       <div className="my-10">
         <img className="w-[290px] my-10" src={logo}/>
         <h2 className='text-center font-semibold text-3xl'>회원가입</h2>
