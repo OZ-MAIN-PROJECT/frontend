e95c4d68ef4a5af1305bb6871ca9a6097c78d465
HBA <goddamncool@naver.com>
2025-05-05 02:00:03 +0900

    feat: #15 커뮤니티 테스트 페이지 재구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
 import MainLayout from './layouts/MainLayout';
 import TestPostCard from './pages/TestPostCard';
-
+import PostDetail from './components/community/PostDetail';
 function App() {
   return (
     <Router>
-      <Route
-        path="/"
-        element={
-          <MainLayout>
-            <HomePage />
-            <TestPostCard />
-          </MainLayout>
-        }
-      />
+      <Routes>
+        {/* 홈 + 테스트 페이지는 한꺼번에 묶어서 렌더링 */}
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <HomePage />
+              <TestPostCard />
+            </MainLayout>
+          }
+        />
+
+        {/* PostDetail도 따로 MainLayout으로 감싸줌 */}
+        <Route
+          path="/community/:id"
+          element={
+            <MainLayout>
+              <PostDetail />
+            </MainLayout>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
