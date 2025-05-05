3d9b34150130ef8e283d6701449b75d68e0bd38f
HBA <goddamncool@naver.com>
2025-05-05 17:45:34 +0900

    feat: #15 커뮤니티 작성자 정보 props 수정

--- src/components/community/AuthorInfo.tsx
+++ src/components/community/AuthorInfo.tsx
@@ -3,15 +3,18 @@ import AuthorProfileImage from './AuthorProfileImage';
 
 interface AuthorInfoProps {
   author: Author;
+  size?: number;
+  fontSize?: string;
+  textColor?: string;
 }
 
-const AuthorInfo = ({ author }: AuthorInfoProps) => {
+const AuthorInfo = ({ author, size = 30, fontSize = 'text-sm', textColor = 'text-primary-800' }: AuthorInfoProps) => {
   const { profileImageUrl, nickname } = author;
 
   return (
     <div className="flex items-center gap-2">
-      <AuthorProfileImage profileImageUrl={profileImageUrl} altText={`${nickname}님의 프로필`} />
-      <span className="text-sm font-medium text-primary-800">{nickname}</span>
+      <AuthorProfileImage profileImageUrl={profileImageUrl} size={size} altText={`${nickname}님의 프로필`} />
+      <span className={`${fontSize} font-medium ${textColor}`}>{nickname}</span>
     </div>
   );
 };
