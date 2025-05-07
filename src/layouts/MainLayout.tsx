8c314d62321f2c7c076894480eca06e506b81929
yurim <kyl6682@gmail.com>
2025-05-07 19:19:39 +0900

    chore : #26 일부 코드 수정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -43,8 +43,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       <Sidebar isVisible={isSidebarVisible} toggleSidebar={toggleSidebar} />
       <div className="flex-1 flex flex-col h-screen">
         <Header toggleSidebar={toggleSidebar} />
-        {/* <main className="flex-1 py-5 lg:py-10">{children}</main> */}
-        <main className={`flex-1 py-5 lg:py-10 ${isSidebarVisible ? 'lg:pl-[250px]' : ''}`}>{children}</main>
+        <main className="flex-1 py-5 lg:py-10">{children}</main>
         <Footer />
       </div>
       {showWalletButton && (
