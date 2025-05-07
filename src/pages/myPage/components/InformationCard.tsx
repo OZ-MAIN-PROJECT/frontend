a53ed46aad70a58574ff549c43ba27436db7f3e6
yurim <kyl6682@gmail.com>
2025-05-07 16:28:10 +0900

    feat : #26 내 정보 프로필 컴포넌트 구현

--- src/pages/myPage/components/InformationCard.tsx
+++ src/pages/myPage/components/InformationCard.tsx
@@ -1,18 +1,16 @@
 import { UserRound } from 'lucide-react';
+import NicknameEditor from './NicknameEditor';
 
 const InformationCard = () => {
   return (
     <div className="flex items-center justify-between">
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 my-4">
         <div className="flex justify-center items-center w-[60px] h-[60px] bg-primary-800 rounded-full mr-1.5">
           <UserRound size={'60px'} className="text-white" />
         </div>
         <div>
-          <p className="font-semibold text-lg">
-            닉네임 <span className="text-sm text-blue-500 cursor-pointer">수정</span>
-          </p>
-          <p className="text-sm text-red-500">이미 사용중인 닉네임입니다. 다른 이름으로 시도해주세요.</p>
-          <p className="text-sm text-gray-500">test123@mail.com</p>
+          <NicknameEditor currentNickname="gildong" />
+          <p className="text-sm text-gray-500">test123@mail.com</p>{' '}
         </div>
       </div>
     </div>
--- src/pages/myPage/components/MyPostsCard.tsx
+++ src/pages/myPage/components/MyPostsCard.tsx
@@ -1,17 +1,5 @@
-const MyPostsCard = () => {
-    return (
-      <div className="flex gap-4">
-        <div className="flex-1 bg-white p-4 rounded shadow text-center">
-          <p className="text-sm text-gray-500">내가 작성한 글</p>
-          <p className="text-xl font-bold">24</p>
-        </div>
-        <div className="flex-1 bg-white p-4 rounded shadow text-center">
-          <p className="text-sm text-gray-500">좋아요 표시한 글</p>
-          <p className="text-xl font-bold">24</p>
-        </div>
-      </div>
-    );
-  };
-  
-  export default MyPostsCard;
-  
+const MyPostCard = () => {
+  return <></>;
+};
+
+export default MyPostCard;
