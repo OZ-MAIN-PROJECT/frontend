8cce2654193e23adaebd973275f7d2cc9e3b5b49
yurim <kyl6682@gmail.com>
2025-05-14 18:04:05 +0900

    fix : #58 오류 수정 후 리빌드

--- src/pages/myPage/components/MyPostsPage.tsx
+++ src/pages/myPage/components/MyPostsPage.tsx
@@ -1,10 +1,8 @@
-import CommunityList from '@/pages/Community/CommunityList';
 
 const MyPostsPage = () => {
   return (
     <div>
       <h2 className="text-3xl font-bold mb-6 ">내정보</h2>
-      <CommunityList />
     </div>
   );
 };
