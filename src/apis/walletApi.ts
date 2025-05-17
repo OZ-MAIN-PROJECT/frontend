86296c7a11c988bef48038e4682c9f159e1435e6
박지예 <jiyea.park0904@gmail.com>
2025-05-17 15:23:39 +0900

    fix: walletApi 코드 수정

--- src/apis/walletApi.ts
+++ src/apis/walletApi.ts
@@ -1,36 +1,24 @@
-
 import { DailyWalletList, MonthlyWalletList, SMonthlyWalletList, SWallet, SWalletList, transformSWalletToWallet, Wallet, WalletList } from "@/types/wallet";
 import api from "./api";
 import { END_POINT } from "@/constants/route";
 import { MonthlyTotal } from "@/types/statistic";
-import {
-  DailyWalletList,
-  MonthlyWalletList,
-  SMonthlyWalletList,
-  SWallet,
-  transformSWalletToWallet,
-  Wallet,
-} from '@/types/wallet';
-import api from './api';
 
 // 가계부 등록
-export const createWalletEntry = async (data: {
+export const createWalletEntry = (data: {
   title: string;
   content: string;
   amount: number;
-  type: 'INCOME' | 'EXPENSE';
+  type: "INCOME" | "EXPENSE";
   walletCategory: string;
   emotion: string;
   date: string;
 }) => {
-
-  const response = await api.post(END_POINT.WALLET, data);
-  return response.data;
+  return api.post(END_POINT.WALLET, data);
 };
 
 // 가계부 리스트/캘린더 조회 (월별)
-export const getWalletMonthly = async (year: number, month: number): Promise<MonthlyWalletList> => {
-  const res = await api.get<SMonthlyWalletList>('/api/wallet', {
+export const getWalletMonthly = async(year: number, month: number): Promise<MonthlyWalletList> => {
+  const res = await api.get<SMonthlyWalletList>(END_POINT.WALLET, {
     params: { year, month },
   });
 
@@ -82,7 +70,7 @@ export const updateWalletEntry = (
     walletCategory: string;
     emotion: string;
     date: string;
-  },
+  }
 ) => {
   return api.patch(END_POINT.WALLET_DETAIL(walletUuid), data);
 };
@@ -93,7 +81,6 @@ export const deleteWalletEntry = (walletUuid: string) => {
 };
 
 // 월별 총 수입/지출 조회
-
 export const getWalletTotal = async(year: number, month: number): Promise<MonthlyTotal> => {
   console.log("월별 총 수입/지출 조회요청:", { year, month });
   const res = await api.get<MonthlyTotal>(END_POINT.WALLET_TOTAL, { params: { year, month } });
