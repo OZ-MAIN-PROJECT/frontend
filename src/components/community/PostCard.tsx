296f65fb219aa5a9f0a1f928798c0eb1496a87b2
yurim <kyl6682@gmail.com>
2025-05-21 02:54:21 +0900

    style : #86 다크모드 적용2

--- src/components/community/PostCard.tsx
+++ src/components/community/PostCard.tsx
@@ -7,7 +7,6 @@ import CommentButton from './CommentButton';
 import AuthorInfo from './AuthorInfo';
 import { Eye } from 'lucide-react';
 import { useLike } from '@/hooks/useLike';
-import altImage from '@/assets/images/altImage.png';
 
 interface ExtendedProps extends PostCardProps {
   viewType: 'list' | 'grid';
@@ -25,12 +24,12 @@ const PostCard = ({ post, viewType, onCommentClick }: ExtendedProps) => {
 
   return (
     <div
-      className={`w-full bg-white dark:bg-gray-700 dark:border-gray-600 rounded-lg border p-5 shadow-sm cursor-pointer ${
+      className={`w-full bg-white dark:bg-gray-700 rounded-lg border p-5 shadow-sm cursor-pointer ${
         viewType === 'list' ? 'flex flex-col gap-2' : 'flex flex-col gap-4'
       }`}
       onClick={() => navigate(`/community/${post.type}/${post.id}`, { state: { type: post.type } })}
     >
-      <div className="text-xs text-primary-500 dark:text-gray-300 flex items-center gap-2">
+      <div className="text-xs text-primary-500 flex items-center gap-2">
         <AuthorInfo author={post.author} />
         <span className="ml-2">{formattedDate}</span>
       </div>
@@ -41,11 +40,11 @@ const PostCard = ({ post, viewType, onCommentClick }: ExtendedProps) => {
             <h2 className="text-base font-semibold text-gray-800 line-clamp-1">{post.title}</h2>
             <p className="text-gray-700 text-sm line-clamp-2">{post.content}</p>
           </div>
-          <div className="flex-shrink-0 rounded-md overflow-hidden w-[100px] h-[100px]">
-            {post.imageUrl && (
-              <img src={post.imageUrl || altImage} alt="게시글 이미지" className="w-full h-full object-cover" />
-            )}
-          </div>
+          {post.imageUrl && (
+            <div className="flex-shrink-0 rounded-md overflow-hidden w-[100px] h-[100px]">
+              <img src={post.imageUrl} alt="게시글 이미지" className="w-full h-full object-cover" />
+            </div>
+          )}
         </div>
       ) : (
         <div className="flex flex-col gap-2">
