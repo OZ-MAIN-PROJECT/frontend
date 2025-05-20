880f22f3f88fa638d2a7ef26ac7468a30a9ee9b8
yurim <kyl6682@gmail.com>
2025-05-20 16:25:47 +0900

    feat : 404 에러 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import CommunityList from './pages/Community/CommunityList';
 import MyPostsPage from './pages/myPage/MyPostsPage';
 import { useDarkModeStore } from './stores/useDarkModeStore';
 import { useEffect } from 'react';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   const setDarkMode = useDarkModeStore((state) => state.setDarkMode);
@@ -50,6 +51,9 @@ function App() {
         <Route path="/community/:type/:postId" element={<MainLayout><PostDetail /></MainLayout>} /> {/* 게시글 상세 먼저! */}
         <Route path="/community/:type" element={<MainLayout><CommunityList /></MainLayout>} /> {/* 게시판 리스트 */}
         <Route path="/community/write" element={<MainLayout><PostWrite /></MainLayout>} />
+
+        {/* 404 처리 라우트 추가 */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
